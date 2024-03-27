486a937e45a43ed2bfa4c437b498ff8e7e80f27f
Ziang Zhu <146101483+ZiangZhu124@users.noreply.github.com>
2024-03-27 19:42:00 -0400

    Add files via upload

--- ZiangZhu_Project2/script.js
+++ ZiangZhu_Project2/script.js
@@ -1,49 +1,267 @@
-const books = [ 
+let artworks =  [ 
     
     
     {
     
-    "title": "Men Without Women",
-    "author": "Haruki Murakami",
-    "summary": "This is the summary of the book",
-    "genre": "Anthology",
-    "bookCover": "https://m.media-amazon.com/images/I/614X9n2mlcL._AC_UF1000,1000_QL80_.jpg",
-    "rating": 4,
+    title: "Teapot",
+    designer: "Marianne Brandt",
+    year:1924,
+    material:"Nickel silver and ebony",
+    image: "https://shop.bauhaus-movement.com/media/2395/catalog/teapot-marianne-brandt.png",
+    category: "Industrial Design",
+    located:"Museum of Modern Art, New York City"
+ 
+    },
+
+
+    {
+    title: "Ceiling Lamp",
+    designer: "Marianne Brandt",
+    year:1925,
+    material:"Spun aluminum and milk glass shade",
+    image: "https://shop.neuegalerie.org/cdn/shop/products/MBPendulumLamp_5Bw_5D_1024x1024.jpeg?v=1605648857",
+    category: "Industrial Design",
+    located:"Museum of Modern Art, New York City"
+     
+    },
+
    
-    "publishData":{
-        "publisher": "Bungeishunju",
-        "yearOfPublish": 2014,
-    },
+    {
     
-    "readingDate":{
-        "start": "02-08-2024",
-        "end": "02-28-2024",
-    },
+        title: "Club Chair Model B3",
+        designer: "Marcel Breuer",
+        year:1927,
+        material:"Chrome-plated tubular steel and canvas",
+        image: "https://modernselections.com/images/products/detail/BlackWassilyChair.1.jpg",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+
+
+     {
+    
+        title: "Armchair",
+        designer: "Marcel Breuer",
+        year:1922,
+        material:"Stained oak and hand-woven wool",
+        image: "https://i.pinimg.com/736x/9c/f3/a7/9cf3a787d0e3febcc804051cd61ad2d0.jpg",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Nesting Tables Model B9",
+        designer: "Marcel Breuer",
+        year:1926,
+        material:"Chrome-plated tubular steel and lacquered plywood",
+        image: "https://shop.bauhaus-movement.com/media/7007/catalog/Thonet_Tisch_SET_B9_FS0_1%20%281%29.jpg",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Cesca Armchair",
+        designer: "Marcel Breuer",
+        year:1928,
+        material:"Bent chromed tubular steel, wood, and cane",
+        image: "https://content.cylindo.com/api/v2/4800/products/51C1/frames/1/51C1.JPG?background=FFFFFF&feature=FRAME%3A:B",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Tea Cart Model B54",
+        designer: "Marcel Breuer",
+        year:1928,
+        material:"Nickel-plated tubular steel, rubber and painted wood",
+        image: "https://www.moma.org/media/W1siZiIsIjIyODg3NSJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=ad03acbe8a5a7bd6",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Chess Set",
+        designer: "Josef Hartwig",
+        year:1924,
+        material:"Pear wood, natural and stained black",
+        image: "https://res.cloudinary.com/fictive-kin/image/upload/v1/verygoods/prod/67b33e294f7f26fd1077741231c39a372ccb9589",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+    
 
     
-},
-
-{
+     {
     
-    "title": "Book2",
-    "author": "Haruki Murakami",
-    "summary": "This is the summary of the book",
-    "genre": "Anthology",
-    "bookCover": "https://m.media-amazon.com/images/I/614X9n2mlcL._AC_UF1000,1000_QL80_.jpg",
-    "rating": 4,
+        title: "Table lamp",
+        designer: "Wilhelm Wagenfeld, Carl Jakob Jucker",
+        year:1924,
+        material:"Chromed metal and glass",
+        image: "https://shop.bauhaus-movement.com/media/2128/catalog/wilhelm-wagenfeld-table-lamp-wa-24.png",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Door Handles",
+        designer: "Walter Gropius, Adolf Meyer",
+        year:1922,
+        material:"Nickel-plated brass",
+        image: "https://image.architonic.com/img_pro2-4/122/3311/walter-gropius-grod23ni-rduni-b.jpg",
+        category: "Industrial Design",
+        located:"Museum of Modern Art, New York City"
+         
+     }
+
+
+];
+
+let graphicDesign=[
    
-    "publishData":{
-        "publisher": "Bungeishunju",
-        "yearOfPublish": 2014,
-    },
+    {
     
-    "readingDate":{
-        "start": "02-08-2024",
-        "end": "02-28-2024",
+        title: "10 Banknotes, designed for the State Bank of Thuringia",
+        designer: "Herbert Bayer",
+        year:1923,
+        material:"Letterpress",
+        image: "https://hogd.pbworks.com/f/1255464023/Bayer_Notgeld_front_Above_sm.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City"
+         
+     },
+
+
+     {
+    
+        title: "Bauhaus Ausstellung Weimar",
+        designer: "Herbert Bayer",
+        year:1923,
+        material:"Lithograph",
+        image: "https://ids.si.edu/ids/deliveryService?id=CHSDM-293373_01-000001",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
     },
 
+
+    {
     
-},
+        title: "Staatliches Bauhaus in Weimar 1919-1923",
+        designer: "Herbert Bayer",
+        year:1923,
+        material:"Lithograph, lithograph",
+        image: "https://www.moma.org/media/W1siZiIsIjE1NzIyMiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=0e020e7ddca09dc1",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+
+    {
+    
+        title: "Dessau, Deutschland, alte Kultur, neue Arbeitsstätten",
+        designer: "Herbert Bayer",
+        year:1926,
+        material:"Letterpress",
+        image: "https://i.pinimg.com/originals/bb/04/08/bb04082769f493392eab62f6b68511c6.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+
+    {
+    
+        title: "Section Allemande",
+        designer: "Herbert Bayer",
+        year:1930,
+        material:"Letterpress",
+        image: "https://live.staticflickr.com/7041/6786712286_b16e4d6014_z.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+    {
+    
+        title: "Ernst Kraus stationery envelope",
+        designer: "Herbert Bayer",
+        year:1922,
+        material:"Letterpress",
+        image: "https://i.pinimg.com/originals/05/9d/2b/059d2bf2732f28ada9389c90ad7509ba.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+   
+    {
+    
+        title: "Staatliches Bauhaus Ausstellung",
+        designer: "Joost Schmidt",
+        year:1923,
+        material:"Letterpress",
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Joost_Schmidt_Bauhausausstellung_1923.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+
+    {
+    
+        title: "Fagus Postkarte",
+        designer: "Herbert Bayer",
+        year:1923,
+        material:"Lithograph",
+        image: "https://i.pinimg.com/originals/e5/18/68/e518688f66ed61042f587810c9cab4cb.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+
+    {
+    
+        title: "Offset Buch und Werbekunst, Jg. 7",
+        designer: "Joost Schmidt",
+        year:1926,
+        material:"letterpress",
+        image: "https://www.lotsearch.de/images/auktion/nosbuesch-stucke.berlin/20160523/724-8.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    },
+
+    {
+    
+        title: "Schlesisches Heim, issue 1/2, 8th year",
+        designer: "Herbert Bayer",
+        year:1927,
+        material:"letterpress",
+        image: "https://i.pinimg.com/736x/51/c4/6e/51c46ee473085dab6a5c37628e8ff7c0.jpg",
+        category: "Graphic Design",
+        located:"Museum of Modern Art, New York City",
+         
+    }
 
 
 
@@ -51,35 +269,160 @@ const books = [
 
 
 
-window.addEventListener('load', ()=>{
-    init();
-})
-
-const init = function(){
-
-    const bookShelf = document.getElementById("bookshelf");
 
 
-    books.map(function(book){
-        const bookItem = buildBookItem(book)
-        bookShelf.innerHTML = bookShelf.innerHTML + bookItem;
-    })
-}
 
-const buildBookItem = function(book){
+let architectureDesign=[
+    {
+    
+        title: "Bauhaus Dessau Campus",
+        designer: "Robert Gropius",
+        year:1925,
+        material:"Concrete, glass and steel beams",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/67/6265_Dessau.JPG",
+        category: "Architecture Design",
+        located:"Dessau, Germany",
+         
+     },
+
+
+     {
+    
+        title: "Monument to the March Dead",
+        designer: "Robert Gropius",
+        year:1925,
+        material:"Concrete",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Monument_to_the_March_dead.jpg",
+        category: "Architecture Design",
+        located:"Weimar, Germany",
+         
+     },
+
+
+     {
+    
+        title: "Fagus Factory",
+        designer: "Robert Gropius, Adolf Meyer",
+        year:1920,
+        material:"Brick, Glass and Steel",
+        image: "https://bauhauskooperation.com/fileadmin/_processed_/7/d/csm_Gropius_Meyer_Fagus-Werk_Alfeld_ac00960a89.jpg",
+        category: "Architecture Design",
+        located:"Alfeld, Germany",
+         
+     },
+
+     {
+    
+        title: "Haus am Horn",
+        designer: "Robert Gropius",
+        year:1923,
+        material:"Concrete anbd glass",
+        image: "https://socks-studio.com/img/blog/haus-am-horn-georg-muche-05.jpg",
+        category: "Architecture Design",
+        located:"Dessau, Germany",
+         
+     },
+
+     {
+    
+        title: "Gropius's House",
+        designer: "Robert Gropius",
+        year:1925,
+        material:"Concrete anbd glass",
+        image: "https://thecharnelhouse.org/wp-content/uploads/2014/04/bauhaus-masters-housing-dessau-1925-1926-directors-house-entrance-view-from-north-architect-walter-gropiusa.jpeg",
+        category: "Architecture Design",
+        located:"Dessau, Germany",
+         
+     },
+
+
+     
+
+
+];
+
+const industrialArtworks = [];
+
+const graphicArtworks = [];
+
+const architectureArtworks=[];
+
+
+
+
+//---------------------------
+
+let archcontainer=document.querySelector (".archcontainer");
+let artcontainer=document.querySelector (".artcontainer");
+let newcontainer=document.querySelector (".newcontainer");
+
+let i=0;
+
+
+
+function displayIndustrial(artworks) {
+    // creates a new div; appends that div to the container
+    let newItem = document.createElement("div");
+    let industrialDiv = archcontainer.appendChild(newItem);
+    // increase the counter variable by one
+    i++;
+    // adds a shared class to each new div
+    newItem.classList.add("indesign");
+    // adds a specific class to each new div
+    newItem.classList.add("indesign" + [i]);
+    // places the flower name and amount to the div
+    industrialDiv.style.backgroundImage = "url(" + artworks.image + ")";
+    // sets height of flower to the amount. try changing height to width, or padding
+    // flowerDiv.style.height = flower.amount + "px";
+    industrialDiv.style.backgroundRepeat = "no-repeat";
+    industrialDiv.style.backgroundSize = "cover";
+
+    console.log(artworks);
+
+  }
+  // loops through the flowers and runs the displayFlowers function for each one
+  artworks.forEach(displayIndustrial);
+
+
+
+  function displayGraphic(graphicDesign) {
+    let newItem = document.createElement("div");
+    let graphicDiv = artcontainer.appendChild(newItem);
+    i++;
+    newItem.classList.add("gradesign");
+    newItem.classList.add("gradesign" + [i]);
+    graphicDiv.style.backgroundImage = "url(" + graphicDesign.image + ")";
+    graphicDiv.style.backgroundRepeat = "no-repeat";
+    graphicDiv.style.backgroundSize = "cover";
+    console.log(graphicDesign);
+  }
   
-    return`<div class="book-item>
-    <img src=${book.bookCover}>
-    <h2> ${book.title}</h2>
-    <h3>${book.author}</h3>
-    <p class="rating">${book.rating}</p>
-    <div class="reading-date>
-    <p>${book.readingDate.start}</p>
-    <p>${book.readingDate.end}<</p>
-    </div>
-    </div>`
+  graphicDesign.forEach(displayGraphic);
 
+
+
+  function displayArch(architectureDesign) {
+    let newItem = document.createElement("div");
+    let archDiv = newcontainer.appendChild(newItem);
+    i++;
+    newItem.classList.add("ardesign");
+    newItem.classList.add("ardesign" + [i]);
+    archDiv.style.backgroundImage = "url(" + architectureDesign.image + ")";
+    archDiv.style.backgroundRepeat = "no-repeat";
+    archDiv.style.backgroundSize = "cover";
+    console.log(architectureDesign);
+  }
+  
+  architectureDesign.forEach(displayArch);
+
+
+
+const buildArtworkItemTest = function(artworks){
+   return `
+   <div class="image-container">
+       <img src="${artworks.image}" class="images">
+   </div>
+   `
 }
-
 
 
